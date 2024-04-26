8a125ffb496ad55cbd57668ed187773a559724a4
xharp <shashalien30@gmail.com>
2024-04-26 14:59:25 +0700

    Practice sampai dengan responsive UI

--- lib/iu/add_review_screen.dart
+++ lib/iu/add_review_screen.dart
@@ -6,12 +6,33 @@ class AddReviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          IconButton(onPressed: () {},
-          icon: Icon(Icons.edit)),
-        ],
-      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return ListView(
+            children: _generateConstraints(),
+          );
+        } else if (constraints.maxWidth < 900) {
+          return GridView.count(
+            crossAxisCount: 2,
+            children: _generateConstraints()
+            );
+        } else {
+          return GridView.count(crossAxisCount: 6
+          );
+        }
+      }
+      )
     );
+  }
+
+  List<Widget> _generateConstraints() {
+    return List<Widget>.generate(10, (index) {
+      return Container(
+        margin: EdgeInsets.all(8),
+        color: Colors.deepPurple[300],
+        height: 200,
+      );
+    });
   }
 }
--- lib/iu/info_screen_detail.dart
+++ lib/iu/info_screen_detail.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
 
 class InfoDetailScreen extends StatelessWidget {
-  const InfoDetailScreen({Key? key}) : super(key: key);
+  const InfoDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    Orientation screenOrientation = MediaQuery.of(context).orientation;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detail Aplikasi Komik'),
-        backgroundColor: Colors.deepPurple[300], // Sesuaikan dengan warna yang diinginkan
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                'BookReviewr',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Text(
-                'Ini adalah deskripsi dan info aplikasi BookReviewr',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            
-            ],
-          ),
-        ),
-      ),
-    );
+        backgroundColor: Colors.deepPurple[300],
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Screen Size: ${screenSize.width.toStringAsFixed(2)}',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'Screen Orientation: $screenOrientation',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        )));
   }
 }
